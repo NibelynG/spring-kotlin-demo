9284961050ecc7009be5cb448fab7cc578c585e6
Eugene Kulikov <e.kulikov@cian.ru>
2021-09-30 14:32:10 +0300

    Fix query

--- src/main/kotlin/com/kulikov/test/kotlindemo/db/repositories/MessageRepository.kt
+++ src/main/kotlin/com/kulikov/test/kotlindemo/db/repositories/MessageRepository.kt
@@ -7,15 +7,9 @@ import org.springframework.data.jpa.repository.Query
 
 interface MessageRepository : JpaRepository<Message, Long> {
 
-//    @Query("select message from Messages message")
-//    fun findAll(): List<Message>
-
-    @Query("select m from Messages m where text = #{text}")
+    @Query("SELECT m from Message m where m.text = ?1")
     fun findAllByText(text: String): List<Message>
 
     fun findByUid(uid: String?): Message?
-
-//    fun <T, ID> CrudRepository<T, ID>.findByIdOrNull(id: ID): T? = findById(id).orElse(null)
-
 
 }
